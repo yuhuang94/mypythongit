3b5147d12ad994052d49756d69aa48191aef7f99
yuhuang94 <a260969472@vip.qq.com>
2017-06-08 22:12:27 +0800

    Revert "python图片链接获取"
    This reverts commit 30e7073d54792e9b81d23b2bf037082c2308e9f9.

--- first.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-from lxml import etree
-url = "http://www.mmjpg.com/mm/1009/2"
-def getlink(html):
-    sele = etree.HTML(html)
-    result = sele.xpath(".//*[@id='content']/a/img/text()")
-    return result
-print(getlink(url))
